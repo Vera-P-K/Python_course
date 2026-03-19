5e16c498cf78a1f59f947a439f2a415c1f7f14b4
Vera-P-K <butylkakefira1997@gmail.com>
2026-03-19 09:42:13 +0500

    Create Task06.py

---
+++ Homework/Mod02/Task06.py
@@ -0,0 +1,14 @@
+s = input("Введите в строку 0 и 1 ")
+zeros = 0
+ones = 0
+
+for char in s:
+    if char == '0':
+        zeros += 1
+    elif char == '1':
+        ones += 1
+
+if zeros == ones:
+    print('yes')
+else:
+    print('no')
