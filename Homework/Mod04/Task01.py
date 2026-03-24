c7e0482fdd22b3c0aa89d97f2bd474b8276fae97
Vera-P-K <butylkakefira1997@gmail.com>
2026-03-24 20:15:18 +0500

    Update Task01.py

--- Homework/Mod04/Task01.py
+++ Homework/Mod04/Task01.py
@@ -1,10 +1,4 @@
-def check_numbers(nums):    
-    unique_nums = set(nums)
-    if len(unique_nums) == 1:
-        return "1) Все числа равны"
-    elif len(unique_nums) == len(nums):
-        return "2) Все числа разные"
-    else:
-        return "3) Есть равные и неравные числа"
-input_data = list(map(float, input().split()))
-print(check_numbers(input_data))
+nums = list(map(float, input().split()))
+u = len(set(nums))
+res = ["1) Все числа равны", "3) Есть равные и неравные числа", "2) Все числа разные"]
+print(res[0 if u == 1 else 2 if u == len(nums) else 1])
