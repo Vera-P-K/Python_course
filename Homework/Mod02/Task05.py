0d75b0ef87f1e61ccca5342303eb11bfe45fba9e
Vera-P-K <butylkakefira1997@gmail.com>
2026-03-24 17:31:01 +0500

    Update Task05.py

--- Homework/Mod02/Task05.py
+++ Homework/Mod02/Task05.py
@@ -1,12 +1,4 @@
-domain = input() 
-parts = []
-current_domain = domain
-while True:
-    dot_index = current_domain.rfind('.')
-    if dot_index == -1:
-        parts.append(current_domain)
-        break
-    parts.append(current_domain[dot_index + 1:])
-    current_domain = current_domain[:dot_index]
-for part in (parts):
+domain = input()
+while domain:
+    domain, dot, part = domain.rpartition('.')
     print(part)
