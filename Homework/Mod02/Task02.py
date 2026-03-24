478264e5000cfccae0c26947edb94145b8f5f812
Vera-P-K <butylkakefira1997@gmail.com>
2026-03-24 16:36:25 +0500

    Update Task02.py

--- Homework/Mod02/Task02.py
+++ Homework/Mod02/Task02.py
@@ -1,7 +1,4 @@
-a = float(input("Введите длину стороны: "))
-perimeter = 2 * (a + a)
-area = a * a
-diagonal = (a**2 + a**2)**0.5
-print(f"Периметр: {round(perimeter, 2)}")
-print(f"Площадь: {round(area, 2)}")
-print(f"Диагональ: {round(diagonal, 2)}")
+a = float(input("Введите длину стороны квадрата: "))
+print(f"Периметр: {round(4 * a, 2)}")
+print(f"Площадь: {round(a**2, 2)}")
+print(f"Диагональ: {round(a * 2**0.5, 2)}")
