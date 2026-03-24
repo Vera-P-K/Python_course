d095d5e84db396baefc5b703fd773ea7364542a8
Vera-P-K <butylkakefira1997@gmail.com>
2026-03-24 20:29:46 +0500

    Update Task02.py

--- Homework/Mod04/Task02.py
+++ Homework/Mod04/Task02.py
@@ -1,13 +1,9 @@
-def fast_power(a, n):
-    if n == 0:
-        return 1
-    if n % 2 == 0:
-        return fast_power(a * a, n // 2)
-    else:
-        return a * fast_power(a, n - 1)
+def power(a, n):
+    if n == 0: return 1
+    return power(a * a, n // 2) if n % 2 == 0 else a * power(a, n - 1)
+
 try:
-    a, n = map(float, input("Введите число и степень через пробел: ").split())
-    result = power(a, int(n))
-    print(f"Результат: {result}")
-except ValueError:
-    print("Пожалуйста, введите корректные числа.")
+    a, n = map(float, input("Введите числа a и n через пробел: ").split())
+    print(f"Результат: {power(a, int(n))}")
+except:
+    print("Ошибка ввода")
